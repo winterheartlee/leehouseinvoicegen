33d0f19fbcf56f7ab0d62cd887976d1868846c0f
winterheartlee <leejonhouse@gmail.com>
2021-09-20 21:46:33 +0000

    Invoice styling improved and if/else statement added to cap rows

--- assets/js/invoiceFunctions.js
+++ assets/js/invoiceFunctions.js
@@ -1,36 +1,44 @@
-// Multiply item price by quanitity and print result upon price field keyup
-$(document.body).on("keyup", ".price-input", function () {
-  let quant = $(this).closest("tr").find(".quant-input").get(0).value;
-  $(this).closest("tr").find(".amount-result").html(this.value * quant);
-});
+$(document).ready(function () {
 
-// Multiply item price by quanitity and print result upon quantity field keyup
-$(document.body).on("keyup", ".quant-input", function () {
-  let price = $(this).closest("tr").find(".price-input").get(0).value;
-  $(this).closest("tr").find(".amount-result").html(this.value * price);
-});
+  // Multiply item price by quanitity and print result upon price field keyup
+  $(document.body).on("keyup", ".price-input", function () {
+    let quant = $(this).closest("tr").find(".quant-input").get(0).value;
+    $(this).closest("tr").find(".amount-result").html(this.value * quant);
+  });
 
-// Add together all individual item prices to create balance total
-$(document.body).on("keyup", ".quant-input, .price-input", function () {
-  let itemTotals = document.getElementsByClassName("amount-result");
-  let sum = 0;
-  for (let i = 0; i < itemTotals.length; i++) {
-    sum += parseInt(itemTotals[i].textContent);
-  }
-  $("#balance").html(sum);
-});
+  // Multiply item price by quanitity and print result upon quantity field keyup
+  $(document.body).on("keyup", ".quant-input", function () {
+    let price = $(this).closest("tr").find(".price-input").get(0).value;
+    $(this).closest("tr").find(".amount-result").html(this.value * price);
+  });
 
-// Add new row to invoice
-$("#appendRow").click(function () {
-  $("#item-table-id").find('tbody').append(`<tr>
-      <td><input type="text" name="item-des" size="50"></td>
-      <td><input type="text" name="item-price" class="price-input" size="8"></td>
-      <td><input type="text" name="item-quant" class="quant-input" size="5" value="1"></td>
-      <td class="amount-result"></td>
-  </tr>`);
-});
+  // Add together all individual item prices to create balance total
+  $(document.body).on("keyup", ".quant-input, .price-input", function () {
+    let itemTotals = document.getElementsByClassName("amount-result");
+    let sum = 0;
+    for (let i = 0; i < itemTotals.length; i++) {
+      sum += parseInt(itemTotals[i].textContent);
+    }
+    $("#balance").html(sum);
+  });
 
-// Delete last row on invoice
-$("#removeRow").click(function () {
-  $("#item-table-id tr:last").remove();
+  // Add new row to invoice
+  document.getElementById("appendRow").onclick = function () {
+    if (document.getElementById("item-table-id").rows.length < 16) {
+      $("#item-table-id").find('tbody').append(`<tr>
+  <td><input type="text" name="item-des" size="80"></td>
+  <td><input type="text" name="item-price" class="price-input" size="8"></td>
+  <td><input type="text" name="item-quant" class="quant-input" size="8" value="1">
+  </td>
+  <td class="amount-result"></td>
+</tr>`);
+    } else {
+      alert("15 item row limit!");
+    }
+  };
+
+  // Delete last row on invoice
+  $("#removeRow").click(function () {
+    $("#item-table-id tr:last").remove();
+  });
 });
