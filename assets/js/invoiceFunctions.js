c0e1d73b878a6a635e81e170869ec8f332dfbf51
winterheartlee <leejonhouse@gmail.com>
2021-09-20 17:32:38 +0000

    Initial commit

---
+++ assets/js/invoiceFunctions.js
@@ -0,0 +1,36 @@
+// Multiply item price by quanitity and print result upon price field keyup
+$(document.body).on("keyup", ".price-input", function () {
+  let quant = $(this).closest("tr").find(".quant-input").get(0).value;
+  $(this).closest("tr").find(".amount-result").html(this.value * quant);
+});
+
+// Multiply item price by quanitity and print result upon quantity field keyup
+$(document.body).on("keyup", ".quant-input", function () {
+  let price = $(this).closest("tr").find(".price-input").get(0).value;
+  $(this).closest("tr").find(".amount-result").html(this.value * price);
+});
+
+// Add together all individual item prices to create balance total
+$(document.body).on("keyup", ".quant-input, .price-input", function () {
+  let itemTotals = document.getElementsByClassName("amount-result");
+  let sum = 0;
+  for (let i = 0; i < itemTotals.length; i++) {
+    sum += parseInt(itemTotals[i].textContent);
+  }
+  $("#balance").html(sum);
+});
+
+// Add new row to invoice
+$("#appendRow").click(function () {
+  $("#item-table-id").find('tbody').append(`<tr>
+      <td><input type="text" name="item-des" size="50"></td>
+      <td><input type="text" name="item-price" class="price-input" size="8"></td>
+      <td><input type="text" name="item-quant" class="quant-input" size="5" value="1"></td>
+      <td class="amount-result"></td>
+  </tr>`);
+});
+
+// Delete last row on invoice
+$("#removeRow").click(function () {
+  $("#item-table-id tr:last").remove();
+});
